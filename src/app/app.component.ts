425f1fcb0fafa0b6cf057b9d346b145b7b8d6143
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-10-27 22:58:40 +0530

    Binding Framework and pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hello-angular';
+  price = 100;
+  discount = 10;
+
+
+  /*updatePrice(price: string) {
+    this.price = +price;
+  }
+
+  updateDiscount(discount: string) {
+    this.discount = +discount;
+  }*/
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { PercentagePipe } from './pipe/percentage.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PercentagePipe
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
