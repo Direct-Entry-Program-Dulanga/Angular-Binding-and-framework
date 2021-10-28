c0e0f4cc7c3fb0cf68bd41e90f02a4450d044264
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-10-28 12:47:53 +0530

    add ngIf and ngFor Directives

--- src/app/structural/structural.component.ts
+++ src/app/structural/structural.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StructuralComponent implements OnInit {
 
+  showParagraph = true;
+  students: Array<{id: string, name: string, rating: number}> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.students.push({id: 'S001', name: 'Manoj', rating: 5});
+    this.students.push({id: 'S003', name: 'Gayal', rating: 4});
+    this.students.push({id: 'S005', name: 'Pethum', rating: 5});
+    this.students.push({id: 'S007', name: 'Dinusha', rating: 2});
+  }
+
+  // toggleVisibility() {
+  //   this.showParagraph = !this.showParagraph;
+  // }
+  reloadStudents() {
+    this.students = [];
+    this.students.push({id: 'S001', name: 'Manoj+', rating: 4});
+    this.students.push({id: 'S003', name: 'Gayal', rating: 5});
+    this.students.push({id: 'S005', name: 'Pethum', rating: 4});
+    this.students.push({id: 'S007', name: 'Dinusha', rating: 5});
+    this.students.push({id: 'S008', name: 'Dinusha', rating: 5});
+  }
+
+  byId(index: number, s: {id: string, name: string}): string {
+    return s.id;
   }
 
 }
