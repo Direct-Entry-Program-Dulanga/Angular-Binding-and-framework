2eeea0e3989e947b05ba878ff2f66c2644ff7fde
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-10-29 02:51:31 +0530

    add parent and child data exchsnge

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,10 +6,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child.component.scss']
 })
 export class ChildComponent implements OnInit {
+  stock = 6;
+  @Output()
+  updateCart = new EventEmitter<number>();
+  @Input()
+  inCart = 0;
 
-  constructor() { }
+  addToCart(): void {
+    if ((this.stock - this.inCart) === 0) return;
 
-  ngOnInit(): void {
+    this.stock--;
+    this.updateCart.emit(1);
   }
 
+  constructor() {
+    console.log("Constructor: ", this.inCart);
+    console.log("Child: Constructor");
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("Child: onChanges", changes);
+  }
+
+  ngOnInit(): void {
+    console.log("ngOnInit: ", this.inCart);
+    console.log("Child: onInit");
+  }
+
+  ngDoCheck(): void {
+    console.log("Child: onDoCheck");
+  }
+
+  ngAfterContentInit(): void {
+    console.log("Child: After Content Init");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("Child: After Content Checked");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("Child: After view Init");
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("Child: After View Checked");
+  }
+
+  ngOnDestroy(): void {
+    console.log("Child: On Destroy")
+  }
 }
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+import {ChildComponent} from "../child/child.component";
 
 @Component({
   selector: 'app-parent',
@@ -7,9 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
-  constructor() { }
+  inCart = 2;
+  @ViewChild(ChildComponent)
+  myChild!: ChildComponent;
+
+  changeCart(emitValue: number) {
+    this.inCart += emitValue;
+  }
+
+  removeFromCart() {
+    if (this.inCart === 0) return;
+    this.inCart--;
+  }
+
+  constructor() {
+    console.log("Constructor");
+    console.log(this.myChild);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("onChanges");
+    console.log(this.myChild);
+  }
 
   ngOnInit(): void {
+    console.log("onInit");
+    // setTimeout(()=>{
+    //   console.log(this.myChild.stock);
+    // }, 100);
+
+  }
+
+  ngDoCheck(): void {
+    console.log("onDoCheck");
+    console.log(this.myChild);
+  }
+
+  ngAfterContentInit(): void {
+    console.log("After Content Init");
+    console.log(this.myChild);
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("After Content Checked");
+    console.log(this.myChild);
+  }
+
+  ngAfterViewInit(): void {
+    console.log("After view Init");
+    console.log(this.myChild);
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("After View Checked");
+  }
+
+  ngOnDestroy(): void {
+    console.log("On Destroy")
   }
 
 }
