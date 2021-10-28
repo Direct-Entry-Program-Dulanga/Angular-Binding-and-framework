983774a5378228e4157bef4d7f2739a0d1ea56c0
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-10-28 12:26:59 +0530

    Add ngmodel with ngmodelchange

--- src/app/styling/styling.component.ts
+++ src/app/styling/styling.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StylingComponent implements OnInit {
 
+  highlight = false;
+  primary = false;
+  underline = false;
+  h1Classes = {};
+
+  // h1Classes = {
+  //   highlight: this.highlight,
+  //   primary: this.primary,
+  //   underline: this.underline
+  // };
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.setH1Classes();
   }
 
+
+  setH1Classes(): void {
+    this.h1Classes = {
+      highlight: this.highlight,
+      primary: this.primary,
+      underline: this.underline
+    }
+  }
 }
